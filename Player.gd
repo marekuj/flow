9cfb99f16e09577b2bc7bffa246a8c87732de667
Piotr Krauzowicz <krauz00@gmail.com>
2018-05-31 04:26:24 +0200

    Tiles one way collisions

--- Player.gd
+++ Player.gd
@@ -8,7 +8,7 @@ const FLOOR_NORMAL = Vector2(0, -1)
 const SLOPE_SLIDE_STOP = 25.0
 const MIN_ONAIR_TIME = 0.1
 const WALK_SPEED = 250 # pixels/sec
-const JUMP_SPEED = 480
+const JUMP_SPEED = 500
 const SIDING_CHANGE_SPEED = 10
 
 var velocity = Vector2()
